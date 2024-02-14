74e4da5c0cce572dddccde3558fac086a5eaf366
fusenattawat <141806694+fusenattawat@users.noreply.github.com>
2024-02-14 09:53:04 +0700

    style.css

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-n
+
+$(document).ready(function() {
+    $(".container").mouseenter(function() {
+        $(".card").stop().animate({
+            top: "-90px"
+        }, "slow");
+    }).mouseleave(function() {
+        $(".card").stop().animate({
+            top: 0
+        }, "slow");
+    });
+});
